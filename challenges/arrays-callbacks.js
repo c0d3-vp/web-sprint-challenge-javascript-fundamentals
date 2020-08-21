869127195bfd38fb1584e43c8b0a1b208bc8a53e
Virginia P. Scirrotto <c0d3.vp@gmail.com>
2020-08-21 11:49:35 -0600

    changes added

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,10 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach((animal) => {
+  displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}.`);
+});
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +33,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map((animal) => {
+  return animal.animal_name.toLowerCase();
+}) 
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,7 +44,11 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+const lowPopulationAnimals = zooAnimals.filter((animal) => {
+  if (animal.population < 5) {
+    return animal.animal_name;
+  }
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -45,38 +56,56 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.reduce((accumulator, next) => {
+  return {population: accumulator.population + next.population};
+})
 console.log(populationTotal);
 
 
-// ==== Callbacks ====  
+// // ==== Callbacks ====  
 
-/* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
+// /* Step 1: Create a higher-order function
+//   * Create a higher-order function named consume with 3 parameters: a, b and cb
+//   * The first two parameters can take any argument (we can pass any value as argument)
+//   * The last parameter accepts a callback
+//   * The consume function should return the invocation of cb, passing a and b into cb as arguments
+// */
+
+function consume(a, b, cb) {
+  return cb(a, b);
+}
+
+// /* Step 2: Create several functions to callback with consume();
+//   * Create a function named add that returns the sum of two numbers
+//   * Create a function named multiply that returns the product of two numbers 
+//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+// */
+
+function add(num1, num2) {
+  return num1 + num2;
+}
 
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
+// /*
 
-/*
+// Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
-
-*/
+// */
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -3,22 +3,33 @@
 /* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
 
 
-const external = "I'm outside the function";
+// const external = "I'm outside the function";
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// function mction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
 
 // Explanation: 
-
+// Even though the variable internal is inside the function myFunction, nestedFunction can access it because of scope.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(param) {
+  let result = 0;
+  for (let i = 0; i < param; i--){
+    result -= param[i];
+  }
+  return result;
+}
+
+console.log(summation(4));
+
